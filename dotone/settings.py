a445b1e63024b25f3b7a4531f70c272885a764f0
mostafa-khodaparast <mostafa.khodaparast1997@gmail.com>
2026-07-28 19:06:17 +0330

    connect project to postgresql

--- dotone/settings.py
+++ dotone/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "dotone.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": DB_NAME,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASS,
+        "HOST": DB_HOST,
+        "PORT": DB_PORT,
     }
 }
 
